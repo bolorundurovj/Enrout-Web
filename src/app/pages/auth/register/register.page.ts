87467bca859450fb58b6de740c085603ed3ccb62
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-09 18:07:12 +0100

    feat(auth): Setup registration UI

--- src/app/pages/auth/register/register.page.ts
+++ src/app/pages/auth/register/register.page.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {RouterModule} from "@angular/router";
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.css'],
 })
