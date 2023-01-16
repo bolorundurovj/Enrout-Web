28dbf7d9b61bd04282e08ffdbf5de58d610d2c6d
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-16 22:16:23 +0100

    refactor(auth): registration page

--- src/app/pages/auth/register/register.page.ts
+++ src/app/pages/auth/register/register.page.ts
@@ -1,11 +1,37 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
 import {RouterModule} from "@angular/router";
+import {UserType} from "@lib/enums/user-type";
+import {FormsModule, NgForm} from "@angular/forms";
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.css'],
 })
-export class RegisterPage {}
+export class RegisterPage {
+  registrationMode: UserType = UserType.STUDENT;
+  userType = UserType;
+  isLoading = false;
+
+  setUserType(type: string): void {
+    switch (type) {
+      case 'staff':
+        this.registrationMode = UserType.STAFF;
+        break;
+      case 'student':
+        this.registrationMode = UserType.STUDENT;
+        break;
+      default:
+        this.registrationMode = UserType.STUDENT;
+    }
+  }
+
+  registerUser(form: NgForm) {
+    this.isLoading = true;
+    if (form.invalid) {
+      return;
+    }
+  }
+}
