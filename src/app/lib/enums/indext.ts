92112296907d76467de39c2d782812286a02c43c
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-22 13:36:09 +0100

    Setup Enums

--- src/app/lib/enums/indext.ts
+++ src/app/lib/enums/indext.ts
@@ -1,1 +1,6 @@
 export * from './user-role.enum';
+export * from './student-type'
+export * from './sort-order'
+export * from './division-type'
+export * from './user-type'
+export * from './staff-designation'
