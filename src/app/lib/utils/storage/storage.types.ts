b91d538e65e47660b62de66ca1d80c29410d1f6e
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-25 21:28:28 +0100

    feat: Update Storage Types

--- src/app/lib/utils/storage/storage.types.ts
+++ src/app/lib/utils/storage/storage.types.ts
@@ -1,14 +1,13 @@
-import { AppTheme } from '@lib/services/theme';
+import {AppTheme} from '@lib/services/theme';
+import {ILoginResponse} from "@lib/interfaces/ilogin-response";
 
 type StorageObjectMap = {
-  'App/session': {
-    user: string;
-    token: string;
-  };
+  'App/session': ILoginResponse;
   'App/theme': AppTheme;
+  'App/token': string;
 };
 
-export type StorageObjectType = 'App/session' | 'App/theme';
+export type StorageObjectType = 'App/session' | 'App/theme' | 'App/token';
 
 export type StorageObjectData<T extends StorageObjectType> = {
   type: T;
