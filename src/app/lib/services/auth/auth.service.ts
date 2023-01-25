a9255f24bfdcd7ee96cf0ad70cf54ceb53fd8ee5
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-25 21:31:16 +0100

    feat: Update Login Functionality

--- src/app/lib/services/auth/auth.service.ts
+++ src/app/lib/services/auth/auth.service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
-import { storage } from '@lib/utils/storage/storage.utils';
-import { BehaviorSubject } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {storage} from '@lib/utils/storage/storage.utils';
+import {BehaviorSubject} from 'rxjs';
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "@env/environment";
+import {ILoginResponse} from "@lib/interfaces/ilogin-response";
+import {IStudentRegistrationPayload} from "@lib/interfaces/istudent-registration-payload";
+import {IStudent} from "@lib/interfaces/student.interface";
+import {IStaffRegistrationPayload} from "@lib/interfaces/istaff-registration-payload";
+import {IStaff} from "@lib/interfaces/istaff";
+import {ILoginPayload} from "@lib/interfaces/ilogin-payload";
+
+const apiUrl = `${environment.apiUrl}/auth`;
 
 @Injectable({
   providedIn: 'root',
@@ -8,17 +19,128 @@ import { BehaviorSubject } from 'rxjs';
 export class AuthService {
   isLoggedIn$ = new BehaviorSubject<boolean>(!!storage.getItem('App/session'));
 
+  constructor(private router: Router, private http: HttpClient) {
+  }
+
   get isLoggedIn(): boolean {
     return this.isLoggedIn$.getValue();
   }
 
-  login(): void {
-    storage.setItem('App/session', { user: 'some-user-id', token: 'abc' });
-    this.isLoggedIn$.next(true);
+  loginStudent(credentials: ILoginPayload): void {
+    const authData = {email: credentials.email, password: credentials.password};
+    this.http
+      .post<ILoginResponse>(
+        apiUrl + '/students/login',
+        authData
+      )
+      .subscribe(
+        (response) => {
+          if (response) {
+            storage.setItem('App/session', response);
+            storage.setItem('App/token', response.token.accessToken);
+            alert(`Logged in successfully`);
+            this.isLoggedIn$.next(true);
+            this.router.navigate(['/student']);
+          } else {
+            alert("An error occurred, please try again")
+          }
+        },
+        (error) => {
+          alert(`${error.error?.error || 'An error occurred'}`);
+          this.isLoggedIn$.next(false);
+        }
+      );
+  }
+
+  loginStaff(credentials: ILoginPayload): void {
+    const authData = {email: credentials.email, password: credentials.password};
+    this.http
+      .post<ILoginResponse>(
+        apiUrl + '/staff/login',
+        authData
+      )
+      .subscribe(
+        (response) => {
+          if (response) {
+            storage.setItem('App/session', response);
+            storage.setItem('App/token', response.token.accessToken);
+            alert(`Logged in successfully`);
+            this.isLoggedIn$.next(true);
+            this.router.navigate(['/staff']);
+          } else {
+            alert("An error occurred, please try again")
+          }
+        },
+        (error) => {
+          alert(`${error.error?.error || 'An error occurred'}`);
+          this.isLoggedIn$.next(false);
+        }
+      );
+  }
+
+  registerStudent(user: IStudentRegistrationPayload): void {
+    const authData = new FormData();
+    for (const key in user) {
+      if (user.hasOwnProperty(key)) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        authData.append(key, user[key]);
+      }
+    }
+    this.http
+      .post<IStudent>(
+        apiUrl + '/students/register',
+        authData
+      )
+      .subscribe(
+        (response) => {
+          if (response) {
+            alert(`Registered successfully`);
+            this.router.navigate(['/auth/login']);
+          } else {
+            alert("An error occurred, please try again")
+          }
+        },
+        (error) => {
+          alert(`${error?.error || 'An error occurred'}`);
+          this.isLoggedIn$.next(false);
+        }
+      );
+  }
+
+  registerStaff(user: IStaffRegistrationPayload): void {
+    const authData = new FormData();
+    for (const key in user) {
+      if (user.hasOwnProperty(key)) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        authData.append(key, user[key]);
+      }
+    }
+    this.http
+      .post<IStaff>(
+        apiUrl + '/staff/register',
+        authData
+      )
+      .subscribe(
+        (response) => {
+          if (response) {
+            alert(`Registered successfully`);
+            this.router.navigate(['/auth/login']);
+          } else {
+            alert("An error occurred, please try again")
+          }
+        },
+        (error) => {
+          alert(`${error?.error || 'An error occurred'}`);
+          this.isLoggedIn$.next(false);
+        }
+      );
   }
 
   logout(): void {
     storage.removeItem('App/session');
+    storage.removeItem('App/token');
     this.isLoggedIn$.next(false);
   }
 }
--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -1,43 +1,52 @@
 import {CommonModule} from '@angular/common';
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router, RouterModule} from '@angular/router';
 import {AuthService} from '@lib/services';
 import {UserType} from "@lib/enums/user-type";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.css'],
 })
-export class LoginPage {
+export class LoginPage implements OnInit {
   loginMode: UserType = UserType.STUDENT;
   userType = UserType;
+  loginForm!: FormGroup;
   private _callbackURL: string;
 
   constructor(
     private _router: Router,
     private _activatedRoute: ActivatedRoute,
     private _authService: AuthService,
+    private fb: FormBuilder
   ) {
     this._callbackURL = this._activatedRoute.snapshot.queryParamMap.get('callbackURL') || `/`;
   }
 
-  setUserType(type: string): void {
-    switch (type) {
-      case 'staff':
-        this.loginMode = UserType.STAFF;
-        break;
-      case 'student':
-        this.loginMode = UserType.STUDENT;
-        break;
-      default:
-        this.loginMode = UserType.STUDENT;
-    }
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      email: [null, [Validators.email, Validators.required]],
+      password: [null, [Validators.required]]
+    })
+  }
+
+  setUserType(type: UserType): void {
+    this.loginMode = type;
   }
 
   onClickSignIn(): void {
-    this._authService.login();
-    this._router.navigate([this._callbackURL]);
+    if (this.loginForm.valid) {
+      if (this.loginMode === this.userType.STAFF) {
+        this._authService.loginStaff(this.loginForm.value);
+      } else {
+        this._authService.loginStudent(this.loginForm.value)
+      }
+    } else {
+      alert('Some data is invalid')
+    }
+    // this._router.navigate([this._callbackURL]);
   }
 }
