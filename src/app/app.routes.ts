2c6714ef8e9cdf00e117da4a8af5e5e224b691fc
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-01-26 11:01:06 +0100

    feat: Add Staff & Student Guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import {Routes} from '@angular/router';
 import {AuthGuard, NoAuthGuard} from '@lib/guards';
 import {StudentLayoutComponent} from "@pages/student/student-layout/student-layout.component";
 import {StaffLayoutComponent} from "@pages/staff/staff-layout/staff-layout.component";
+import {StaffGuard} from "@lib/guards/staff.guard";
+import {StudentGuard} from "@lib/guards/student.guard";
 
 export const routes: Routes = [
   {
@@ -22,12 +24,14 @@ export const routes: Routes = [
   {
     path: 'staff',
     component: StaffLayoutComponent,
-    loadChildren: async () => (await import('@pages/staff/staff.routes')).ROUTES
+    loadChildren: async () => (await import('@pages/staff/staff.routes')).ROUTES,
+    canLoad: [AuthGuard, StaffGuard]
   },
   {
     path: 'student',
     component: StudentLayoutComponent,
-    loadChildren: async () => (await import('@pages/student/student.routes')).ROUTES
+    loadChildren: async () => (await import('@pages/student/student.routes')).ROUTES,
+    canLoad: [AuthGuard, StudentGuard]
   },
   {
     path: ':username',
