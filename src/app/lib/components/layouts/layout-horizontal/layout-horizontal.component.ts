cb0326e80630b5a9c02ffde69e775aad5ecb78b1
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-02-09 19:54:22 +0100

    feat: Refactor App Layout

--- src/app/lib/components/layouts/layout-horizontal/layout-horizontal.component.ts
+++ src/app/lib/components/layouts/layout-horizontal/layout-horizontal.component.ts
@@ -1,9 +1,86 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SidebarService} from "flowbite-angular";
+import {AuthService} from "@lib/services";
+import {UserType} from "@lib/enums/user-type";
 
 @Component({
   selector: 'app-layout-horizontal',
   templateUrl: './layout-horizontal.component.html',
   styleUrls: ['./layout-horizontal.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LayoutHorizontalComponent {}
+export class LayoutHorizontalComponent {
+  showProfile = false;
+  showNotifications = false;
+  staffRoutes: RouteProps[] = [
+    {
+      title: 'Dashboard',
+      href: '/staff/dashboard',
+      group: false,
+      icon: `<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>`,
+    },
+    {
+      title: 'Submissions',
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">\n  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />\n</svg>',
+      href: '/staff/documents',
+      group: false,
+      card: {
+        className: 'w-56',
+        images: {light: 'accordion-light.svg', dark: 'accordion-dark.svg'},
+      },
+    },
+  ];
+
+  studentRoutes: RouteProps[] = [
+    {
+      title: 'Dashboard',
+      href: '/student/dashboard',
+      group: false,
+      icon: `<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>`,
+    },
+    {
+      title: 'Submissions',
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">\n  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />\n</svg>',
+      href: '/student/documents',
+      group: false,
+      card: {
+        className: 'w-56',
+        images: {light: 'accordion-light.svg', dark: 'accordion-dark.svg'},
+      },
+    },
+  ];
+
+  components: RouteProps[] = this.authService.loggedInUserType === UserType.STAFF ? this.staffRoutes : this.studentRoutes;
+
+  user = this.authService.loggedInUser;
+
+
+  constructor(readonly sidebarService: SidebarService, private readonly authService: AuthService) {
+  }
+
+  displayNotifications() {
+    this.showProfile = false;
+    this.showNotifications = !this.showNotifications
+  }
+
+  displayProfile() {
+    this.showNotifications = false;
+    this.showProfile = !this.showProfile
+  }
+
+  logout() {
+    this.authService.logout()
+  }
+}
+
+export type ComponentCardItem = {
+  className: string;
+  images: { light: string; dark: string };
+};
+
+export type RouteProps = {
+  title: string;
+  icon: string;
+  href: string;
+  group: boolean;
+  card?: ComponentCardItem;
+};
