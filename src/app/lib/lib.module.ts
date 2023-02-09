4da4d38a7c3573801bc58c209cd77bcf418b79bc
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-02-09 21:27:31 +0100

    perf: Remove Unused Lib Components

--- src/app/lib/lib.module.ts
+++ src/app/lib/lib.module.ts
@@ -4,21 +4,19 @@ import {LogoComponent} from "@lib/components/logo/logo.component";
 import {ModalComponent} from "@lib/components/modal/modal.component";
 import {RouterModule} from "@angular/router";
 import {BackdropComponent} from "@lib/components/backdrop/backdrop.component";
-import {FooterComponent} from './components/footer/footer.component';
-import {NavbarComponent} from "@lib/components/navbar/navbar.component";
 import {LayoutHorizontalComponent} from "@lib/components/layouts/layout-horizontal/layout-horizontal.component";
 import {SpinnerComponent} from './components/spinner/spinner.component';
 import {FlowbiteModule} from "flowbite-angular";
 
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent, LogoComponent, ModalComponent, BackdropComponent, LayoutHorizontalComponent, SpinnerComponent],
+  declarations: [LogoComponent, ModalComponent, BackdropComponent, LayoutHorizontalComponent, SpinnerComponent],
   imports: [
     CommonModule,
     RouterModule,
     FlowbiteModule
   ],
-  exports: [NavbarComponent, FooterComponent, LogoComponent, ModalComponent, BackdropComponent, LayoutHorizontalComponent, SpinnerComponent],
+  exports: [LogoComponent, ModalComponent, BackdropComponent, LayoutHorizontalComponent, SpinnerComponent],
 })
 export class LibModule {
 }
