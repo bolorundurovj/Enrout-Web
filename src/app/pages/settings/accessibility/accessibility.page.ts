014dd0354334281b1a7108bed02da1be05474e92
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-02-09 21:24:47 +0100

    perf: Remove Unused Settings Components

--- src/app/pages/settings/accessibility/accessibility.page.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './accessibility.page.html',
-  styleUrls: ['./accessibility.page.css'],
-})
-export class AccessibilityPage {}
--- src/app/pages/settings/settings-routing.module.ts
+++ src/app/pages/settings/settings-routing.module.ts
@@ -1,29 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {AccessibilityPage} from "@pages/settings/accessibility/accessibility.page";
-import {AccountPage} from "@pages/settings/account/account.page";
-import {AppearancePage} from "@pages/settings/appearance/appearance.page";
 
-const routes: Routes = [{
-  path: '',
-  redirectTo: 'account',
-  pathMatch: 'full'
-},
-  {
-    path: 'accessibility',
-    title: 'Accessibility settings',
-    component: AccessibilityPage,
-  },
-  {
-    path: 'account',
-    title: 'Account settings',
-    component: AccountPage,
-  },
-  {
-    path: 'appearance',
-    title: 'Appearance settings',
-    component: AppearancePage,
-  },];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
